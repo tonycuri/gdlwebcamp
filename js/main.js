2edd2b966ca38b3ac8ea77bf727f1e04a23d910f
Tony Curi <curicaballero@gmail.com>
2020-06-18 19:41:16 -0300

    declaramos las variables

--- js/main.js
+++ js/main.js
@@ -2,6 +2,21 @@
   'use strict';
 
   document.addEventListener('DOMContentLoaded', function(){
+    //campos datos usuarios
+    var nombre = document.getElementById('nombre');
+    var apellido = document.getElementById('apellido');
+    var email = document.getElementById('email');
+
+    //campos pases
+    var pase_dia = document.getElementById('pase_dia');
+    var pase_dosdias = document.getElementById('pase_dosdias');
+    var pase_completo = document.getElementById('pase_completo');
+
+    //botones y divs
+    var calcular = document.getElementById('calcular');
+    var errorDiv = document.getElementById('error');
+    var botonRegistro = document.getElementById('btnRegistro');
+    var resultado = document.getElementById('lista-productos');
 
   });//DOM CONTENT LOADED
 })();
