455df82076bf559570089a38d670b320a8104b6d
Tony Curi <curicaballero@gmail.com>
2020-06-21 00:32:05 -0300

    generamos que la funcion calcularMontos imprima el resumen de los gastos

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@
     var calcular = document.getElementById('calcular');
     var errorDiv = document.getElementById('error');
     var botonRegistro = document.getElementById('btnRegistro');
-    var resultado = document.getElementById('lista-productos');
+    var lista_productos = document.getElementById('lista-productos');
 
     //extras
     var etiquetas = document.getElementById('etiquetas');
@@ -32,15 +32,35 @@
         alert('Debes elegir un regalo');
         regalo.focus();
       }else{
-        var boletosDia = pase_dia.value,
-            boleto2Dias= pase_dosdias.value,
-            boletoCompleto = pase_completo.value,
-            cantCamisas = camisas.value,
-            cantEtiquetas = etiquetas.value;
+        var boletosDia = parseInt(pase_dia.value,10)|| 0,
+            boleto2Dias= parseInt(pase_dosdias.value)|| 0,
+            boletoCompleto = parseInt(pase_completo.value)|| 0,
+            cantCamisas = parseInt(camisas.value)|| 0,
+            cantEtiquetas = parseInt(etiquetas.value)|| 0;
 
         var totalPagar = (boletosDia * 30) + (boleto2Dias * 45) + (boletoCompleto * 50) + ((cantCamisas * 10)* .93) + (cantEtiquetas * 2);
-        console.log(totalPagar);
 
+        var listadoProductos = [];
+        if(boletosDia >=1){
+          listadoProductos.push(boletosDia + 'Pases por día');
+        }
+        if(boleto2Dias >= 1){
+          listadoProductos.push(boleto2Dias + 'Pases por 2 días');
+        }
+        if(boletoCompleto >=1){
+          listadoProductos.push(boletoCompleto + 'Pases completos');
+        }
+        if(cantCamisas >=1){
+          listadoProductos.push(cantCamisas + 'Camisas');
+        }
+        if(cantEtiquetas >=1){
+          listadoProductos.push(cantEtiquetas + 'Etiquetas');
+        }
+
+        lista_productos.innerHTML = '';
+        for(var i = 0; i<listadoProductos.length; i++){
+          lista_productos.innerHTML += listadoProductos[i]+ '</br>';
+        }
       }
     }
 
