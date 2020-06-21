2d30f82e47f493da26434d835ceae1015307ad59
Tony Curi <curicaballero@gmail.com>
2020-06-20 22:23:28 -0300

    obtenemos la cantidad de camisas y etiquetas y las sumamos en el totalPagar dentro de la funcion calcularMontos

--- js/main.js
+++ js/main.js
@@ -20,6 +20,10 @@
     var botonRegistro = document.getElementById('btnRegistro');
     var resultado = document.getElementById('lista-productos');
 
+    //extras
+    var etiquetas = document.getElementById('etiquetas');
+    var camisas = document.getElementById('camisa_evento');
+
     calcular.addEventListener('click',calcularMontos);
 
     function calcularMontos(event){
@@ -30,9 +34,11 @@
       }else{
         var boletosDia = pase_dia.value,
             boleto2Dias= pase_dosdias.value,
-            boletoCompleto = pase_completo.value;
+            boletoCompleto = pase_completo.value,
+            cantCamisas = camisas.value,
+            cantEtiquetas = etiquetas.value;
 
-        var totalPagar = (boletosDia * 30) + (boleto2Dias * 45) + (boletoCompleto * 50);
+        var totalPagar = (boletosDia * 30) + (boleto2Dias * 45) + (boletoCompleto * 50) + ((cantCamisas * 10)* .93) + (cantEtiquetas * 2);
         console.log(totalPagar);
 
       }
