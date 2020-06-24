8c2d3411b40975313cc671f0ecaacf8692b3ca08
Tony Curi <curicaballero@gmail.com>
2020-06-24 20:26:53 -0300

    solucionamos el error con el mapa

--- js/main.js
+++ js/main.js
@@ -5,15 +5,16 @@
 
   document.addEventListener('DOMContentLoaded', function(){
     //agregamos el mapa
-    var map = L.map('mapa').setView([51.505, -0.09], 13);
+    if(document.getElementById('mapa')){
+      var map = L.map('mapa').setView([-34.674123, -58.458976], 17);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+        L.marker([-34.674123, -58.458976]).addTo(map)
+        .bindPopup('GDLWebcamp entradas disponibles')
+        .openPopup();
+    }
 
     //campos datos usuarios
     var nombre = document.getElementById('nombre');
